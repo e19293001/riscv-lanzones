5f2fe5ff8ffd623abc45795230c91fae4ab020ed
e19293001 <e19293001@gmail.com>
2017-01-19 13:29:09 +0800

    updated asmblr but not yet finished

--- bin/asmblr.py
+++ bin/asmblr.py
@@ -1,36 +1,126 @@
 EOF = 0
+UNSIGNED = 1
+HEX = 2
+LUI = 3
+ADD = 4
+SW = 5
+LW = 6
+ERROR = 7
+COMMA = 8
 
 class Token:
-    kind = EOF
-    beginLine = 0
-    beginColumn = 0
-    endLine = 0
-    endColumn = 0
-    image = ""
+    def __init__(self):
+        self.kind = EOF
+        self.beginLine = 0
+        self.beginColumn = 0
+        self.endLine = 0
+        self.endColumn = 0
+        self.image = ""
     
 
 class TokenMgr:
-    inFile = ""
-    outFile = ""
-    currentChar = ""
-    currentColumnNumber = 0
-    currentLineNumber = 0
-    inputLine = ""
 
     def __init__(self, inFile, outFile):
         self.inFile = inFile
         self.outFile = outFile
+        self.inFileHandle = open(inFile, "r")
+        self.outFileHandle = open(outFile, "w")
+        self.currentChar = "\n"
+        self.currentColumnNumber = 0
+        self.currentLineNumber = 0
+        self.inputLine = ""
+        self.buff = ""
+        print "started"
+
+    def __del__(self):
+        self.inFileHandle.close()
+        self.outFileHandle.close()
+        print "finished"
 
     def getNextToken(self):
+        while self.currentChar.isspace():
+            self.getNextChar()
+            
         tkn = Token()
-        tkn.beginLine = currentLineNumber
-        tkn.beginColumn = currentColumnNumber
+        tkn.beginLine = self.currentLineNumber
+        tkn.beginColumn = self.currentColumnNumber
         tkn.kind = EOF
+
+        if self.currentChar == EOF:
+            tkn.image = "<EOF>"
+            tkn.endLine = self.currentLineNumber
+            tkn.endColumn = self.currentColumnNumber
+            tkn.kind = EOF
+        else:
+            if self.currentChar.isdigit():
+                if self.currentChar == "0":
+                    self.buff = ""
+                    while True:
+                        self.buff = self.buff + self.currentChar
+                        tkn.endLine = self.currentLineNumber
+                        tkn.endColumn = self.currentColumnNumber
+                        self.getNextChar()
+                        if not self.currentChar.isdigit():
+                            break
+                        tkn.image = self.buff
+                        tkn.kind = HEX
+            elif self.currentChar.isalnum():
+                self.buff = ""
+                while True:
+                    self.buff = self.buff + self.currentChar
+                    tkn.endLine = self.currentLineNumber
+                    tkn.endColumn = self.currentColumnNumber
+                    #if not (self.currentChar.isalnum() and self.currentChar == "x"):
+                    if self.currentChar.isalnum():
+                        break
+                tkn.image = self.buff
+                if tkn.image == "LUI":
+                    tkn.kind = LUI
+                elif tkn.image == "ADD":
+                    tkn.kind = ADD
+                elif tkn.image == "SW":
+                    tkn.kind = SW
+                elif tkn.image == "LW":
+                    tkn.kind = LW
+                else:
+                    tkn.kind = ERROR
+            elif self.currentChar == ",":
+                tkn.endLine = self.currentLineNumber
+                tkn.endColumn = self.currentColumnNumber
+                self.getNextChar()
+                tkn.image = self.currentChar
+                tkn.kind = COMMA
+            else:
+                tkn.kind = ERROR
+        return tkn
+
+    def getNextChar(self):
+        if self.currentChar == EOF:
+            print "reached EOF"
+            return
+
+        if self.currentChar == "\n":
+            self.inputLine = self.inFileHandle.readline()
+            if self.inputLine:
+                print "inputLine:" + self.inputLine
+                self.currentColumnNumber = 0
+                self.currentLineNumber = self.currentLineNumber + 1
+            else:
+                self.currentChar = EOF
+                return
+
+        self.currentChar = self.inputLine[self.currentColumnNumber]
+        self.currentColumnNumber = self.currentColumnNumber + 1
+        #print "currentChar:" + self.currentChar
         
-tmgr = TokenMgr("infile.txt", "outfile.txt")
 
+tmgr = TokenMgr("tstPattern0003.asm", "outfile.txt")
+tmgr.getNextChar()
 
-
+for i in range(10):
+    tkn = tmgr.getNextToken()
+    print "Token.image:" + tkn.image
+    print "Token.kind:" + str(tkn.kind)
 
 #import sys
 #import re
