5e094241a42518c5edabfde7d619e59544fa8da7
e19293001 <e19293001@gmail.com>
2017-01-21 22:30:13 +0800

    fixed getNextToken in tokenmgr

--- bin/asmblr.py
+++ bin/asmblr.py
@@ -7,6 +7,7 @@ SW = 5
 LW = 6
 ERROR = 7
 COMMA = 8
+REGISTER = 9
 
 class Token:
     def __init__(self):
@@ -16,6 +17,41 @@ class Token:
         self.endLine = 0
         self.endColumn = 0
         self.image = ""
+
+        self.tokenImage = ["EOF",
+          "UNSIGNED",
+          "HEX",
+          "LUI",
+          "ADD",
+          "SW",
+          "LW",
+          "ERROR",
+          "COMMA",
+          "REGISTER"]
+          
+    def getKind(self,k):
+        if (k == 0):
+            return "EOF"
+        elif (k == 1):
+            return "UNSIGNED"
+        elif (k == 2):
+            return "HEX"
+        elif (k == 3):
+            return "LUI"
+        elif (k == 4):
+            return "ADD"
+        elif (k == 5):
+            return "SW"
+        elif (k == 6):
+            return "LW"
+        elif (k == 7):
+            return "ERROR"
+        elif (k == 8):
+            return "COMMA"
+        elif (k == 9):
+            return "REGISTER"
+        else:
+            return "UNKNOWN"
     
 
 class TokenMgr:
@@ -44,27 +80,26 @@ class TokenMgr:
         tkn = Token()
         tkn.beginLine = self.currentLineNumber
         tkn.beginColumn = self.currentColumnNumber
-        tkn.kind = EOF
 
-        if self.currentChar == EOF:
+        if self.currentChar == "EOF":
             tkn.image = "<EOF>"
             tkn.endLine = self.currentLineNumber
             tkn.endColumn = self.currentColumnNumber
             tkn.kind = EOF
+            return tkn
         else:
             if self.currentChar.isdigit():
                 if self.currentChar == "0":
-                    #print "found 0"
                     self.buff = ""
                     while True:
                         self.buff = self.buff + self.currentChar
                         tkn.endLine = self.currentLineNumber
                         tkn.endColumn = self.currentColumnNumber
                         self.getNextChar()
-                        if not self.currentChar.isdigit():
+                        if not (self.currentChar.isdigit() or self.currentChar == "x"):
                             break
-                        tkn.image = self.buff
-                        tkn.kind = HEX
+                    tkn.image = self.buff
+                    tkn.kind = HEX
             elif self.currentChar.isalnum():
                 self.buff = ""
                 while True:
@@ -85,10 +120,11 @@ class TokenMgr:
                     tkn.kind = SW
                 elif tkn.image == "LW":
                     tkn.kind = LW
+                elif tkn.image[0] == "x" and tkn.image[1:].isdigit():
+                    tkn.kind = REGISTER
                 else:
                     tkn.kind = ERROR
             elif self.currentChar == ",":
-                print "comma"
                 tkn.endLine = self.currentLineNumber
                 tkn.endColumn = self.currentColumnNumber
                 tkn.image = self.currentChar
@@ -99,18 +135,18 @@ class TokenMgr:
         return tkn
 
     def getNextChar(self):
-        if self.currentChar == EOF:
+        if self.currentChar == "EOF":
             print "reached EOF"
             return
 
         if self.currentChar == "\n":
             self.inputLine = self.inFileHandle.readline()
             if self.inputLine:
-                print "inputLine:" + self.inputLine
+                print "\ninputLine:" + self.inputLine
                 self.currentColumnNumber = 0
                 self.currentLineNumber = self.currentLineNumber + 1
             else:
-                self.currentChar = EOF
+                self.currentChar = "EOF"
                 return
 
         self.currentChar = self.inputLine[self.currentColumnNumber]
@@ -121,72 +157,7 @@ class TokenMgr:
 tmgr = TokenMgr("tstPattern0003.asm", "outfile.txt")
 tmgr.getNextChar()
 
-for i in range(10):
+for i in range(50):
     tkn = tmgr.getNextToken()
-    print "Token.image:" + tkn.image + " Token.kind:" + str(tkn.kind)
-
-#import sys
-#import re
-#
-#def instformat(s,i):
-#    return "{0:0{1}x}".format(int(s,2),i)
-#
-#def binformat(s,i):
-#    return s.zfill(i)
-#
-#def tobinstr(spltd):
-#    return str(bin(int(spltd)))[2:]
-#
-#def printHelp():
-#    print "usage: python2 ../bin/asmblr.py <asm file>"
-#
-#def getReg(ireg):
-#    for i in range(32):
-##        print "x" + str(i)
-#        if ireg == ("x" + str(i)):
-##            print "Match:" + ireg
-#            return i
-#
-#print "number of arguments:", len(sys.argv)
-#
-#if len(sys.argv) == 1:
-#    printHelp()
-#    sys.exit()
-#
-#asmfile = sys.argv[1]
-#
-#print "asmfile:" + asmfile
-#
-#with open(asmfile, "r") as asmhandle:
-#    for line in asmhandle:
-#        linestrp = line.strip()
-#        print "; " + line
-#        spltd = re.split(',| ',linestrp)
-#        opcode = ""
-#        rd = ""
-#        imm = ""
-#        rs1 = ""
-#        rs2 = ""
-#
-#        if (spltd[0] == "LUI"):
-#            opcode = "0110111"
-#            rd = tobinstr(getReg(spltd[1]))
-#            if (spltd[2][0] == "0" and spltd[2][1] == "x"):
-#                imm = tobinstr(spltd[2][2:])
-#            else:
-#                imm = "0"
-#            #print imm, rd, opcode
-#            #print binformat(imm,12), binformat(rd,5), binformat(opcode,7)
-#            instruction = binformat(imm,12) + binformat(rd,5) + binformat(opcode,7)
-#            print instformat(instruction,8)
-#        elif (spltd[0] == "ADD"):
-#            opcode = "0110011"
-#            rd = tobinstr(getReg(spltd[1]))
-#            rs1 = tobinstr(getReg(spltd[2]))
-#            rs2 = tobinstr(getReg(spltd[3]))
-#            #instruction = binformat(rd,5) + binformat(rs1,5) + binformat(rs2,5) + binformat(opcode,7)
-#            instruction = "0000000" + binformat(rs2,5) + binformat(rs1,5) + "000" + binformat(rd,5) + binformat(opcode,7)
-#            print instruction
-#            #print opcode, rd, rs1, rs2
-#            print instformat(instruction,8)
+    print "Token.image:" + tkn.image + " Token.kind:" + tkn.getKind(tkn.kind)
 
