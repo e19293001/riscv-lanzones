2ce7c02d6e8f27c8c8a37011d3f6e15e11d04ea8
e19293001 <e19293001@gmail.com>
2017-03-13 10:53:35 +0800

    added label support for SLTI instruction

--- bin/asmblr.py
+++ bin/asmblr.py
@@ -591,7 +591,6 @@ class asmblr:
                 immstr = self.hextobinstr(str(hex(self.symboltablename[imm.image])))
                 rdstr = self.tobinstr(rd.image[1:])
                 instruction = self.binformat(immstr,20) + self.binformat(rdstr,5) + self.binformat(op,7)
-                print "LUIpattern: " + self.instformat(instruction,8)
                 self.cg.emitInstruction(self.programcounter, self.instformat(instruction,8))
                 self.consume(ID)
             else:
@@ -879,12 +878,32 @@ class asmblr:
 
         rdstr = self.tobinstr(rd.image[1:])
         rs1str = self.tobinstr(rs1.image[1:])
-        immstr = self.hextobinstr(imm.image[2:])
-        
-        instruction = self.binformat(immstr,12) + self.binformat(rs1str,5) + "010" + self.binformat(rdstr,5) + self.binformat(op,7)
 
-        self.cg.emitInstruction(self.programcounter, self.instformat(instruction,8))
-        self.consume(HEX)
+        if self.asmblrstate == PARSESTATE_ASM:
+            if imm.kind == HEX:
+                immstr = self.hextobinstr(imm.image[2:])
+                
+                instruction = self.binformat(immstr,12) + self.binformat(rs1str,5) + "010" + self.binformat(rdstr,5) + self.binformat(op,7)
+
+                self.cg.emitInstruction(self.programcounter, self.instformat(instruction,8))
+                self.consume(HEX)
+            elif imm.kind == ID:
+                immstr = self.hextobinstr(str(hex(self.symboltablename[imm.image])))
+                instruction = self.binformat(immstr,12) + self.binformat(rs1str,5) + "010" + self.binformat(rdstr,5) + self.binformat(op,7)
+
+                self.cg.emitInstruction(self.programcounter, self.instformat(instruction,8))
+                self.consume(ID)
+            else:
+                print "Error. Hex value or a label is expected."
+                exit(1)
+        elif self.asmblrstate == PARSESTATE_LABELS:
+            if imm.kind == HEX:
+                self.consume(HEX)
+            elif imm.kind == ID:
+                self.consume(ID)
+        else:
+            print "Error. Invalid state"
+            exit(1)
 
     def SLTIUpattern(self):
         op = "0010011"
