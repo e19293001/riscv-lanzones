75339ddb529136316e50305d19a105510c1c57df
e19293001 <e19293001@gmail.com>
2017-04-06 19:35:25 +0800

    fixed bug on bst_search; simulation can now run

--- sim/tstvpiassoc.v
+++ sim/tstvpiassoc.v
@@ -3,18 +3,36 @@ module test;
    reg [31:0] address;
    initial begin
       address = 'hFFFFFFFF;
-      $dynmem_write(address,4);
+      //$dynmem_write(address,4);
+      //
+      //data = $dynmem_read(address);
+      //$display("data after read: %0x", data);
+      //
+      //address = 'hABCDE123;
+      //$dynmem_write(address,'h444444);
+      //
+      //data = $dynmem_read(address);
+      //$display("data after read: %0x", data);
+      //
+      //$display("reading @00000888");
+      //$display("%08x", $dynmem_read('h00000888));
 
-      data = $dynmem_read(address);
-      $display("data after read: %0x", data);
+      //+00000000 FFFFF237
+      //+00000001 0000126F
+      //+00000002 FFFFFFFF
 
-      address = 'hABCDE123;
-      $dynmem_write(address,'h444444);
-
-      data = $dynmem_read(address);
-      $display("data after read: %0x", data);
-
-      $display("reading @00000888");
-      $display("%08x", $dynmem_read('h00000888));
+      address = 'h00000000;
+      data = 'hFFFFF237;
+      $dynmem_write(address,data);
+      address = 'h00000001;
+      data = 'h0000126F;
+      $dynmem_write(address,data);
+      address = 'h00000002;
+      data = 'hFFFFFFFF;
+      $dynmem_write(address,data);
+      $display("%08x", $dynmem_read('h00000000));
+      $display("%08x", $dynmem_read('h00000001));
+      $display("%08x", $dynmem_read('h00000002));
+      $display("%08x", $dynmem_read('h00000003));
    end
 endmodule
--- vpi/bst.c
+++ vpi/bst.c
@@ -10,16 +10,30 @@ node *bst_new(mem memdata) {
   return nd;
 }
 
+void bst_traverse(node *n) {
+  if (n == NULL) {
+    //printf("[ bst_traverse ] returning\n");
+  }
+  else {
+    bst_traverse(n->left);
+    printf("[ bst_traverse ] address: %08X data: %08X\n", n->data.address, n->data.data);
+    bst_traverse(n->right);
+  }
+}
+
 node *bst_insert(node *n, mem memdata) {
   if (n == NULL) {
+    printf("[ bst_insert ] n is NULL creating new\n");
     node *nd = bst_new(memdata);
     return nd;
   }
   else {
     if (memdata.address <= n->data.address) {
+      printf("[ bst_insert ] [ going to LEFT ] memdata.address: %0d\n", memdata.address);
       n->left = bst_insert(n->left, memdata);
     }
     else {
+      printf("[ bst_insert ] [ going to RIGHT ] memdata.address: %0d\n", memdata.address);
       n->right = bst_insert(n->right, memdata);
     }
     return n;
@@ -37,13 +51,11 @@ void bst_free(node *n) {
 }
 
 unsigned int bst_search(node *n, mem memdata) {
-//  printf("searching for mem.address: %08X\n", memdata.address);
-//  if (n == NULL || memdata.address == n->data.address) {
   if (n == NULL) {
-//  if (memdata.address == n->data.address) {
-//    printf("returning data: %0d\n", n->data.data);
-//    return n->data.data;
     return 0;
+  }
+  if (memdata.address == n->data.address) {
+    return n->data.data;
   }
   else if (memdata.address < n->data.address) {
     bst_search(n->left, memdata);
@@ -51,26 +63,45 @@ unsigned int bst_search(node *n, mem memdata) {
   else if (memdata.address > n->data.address) {
     bst_search(n->right, memdata);
   }
-  else {
-    return n->data.data;
+//  printf("searching for mem.address: %08X\n", memdata.address);
+////  if (n == NULL || memdata.address == n->data.address) {
+//  if (n == NULL) {
+////  if (memdata.address == n->data.address) {
+////    printf("returning data: %0d\n", n->data.data);
+////    return n->data.data;
 //    return 0;
-  }
-  return 0;
+//  }
+//  else if (memdata.address < n->data.address) {
+//    bst_search(n->left, memdata);
+//  }
+//  else if (memdata.address > n->data.address) {
+//    bst_search(n->right, memdata);
+//  }
+//  else {
+//    printf("FOUND %08X\n", n->data.data);
+//    return n->data.data;
+////    return 0;
+//  }
+//  return 0;
 }
 
 #ifdef DEBUG_BST
 void testsearch(node *bst, mem memdata) {
-  if (bst_search(bst,memdata) == 1) {
-    printf("%0d found\n", memdata.address);
-  }
-  else {
-    printf("%0d not found\n", memdata.address);
-  }
+  printf("testing search...\n");
+  printf("                  address: %08X\n", memdata.address);
+  printf("                  data: %08X\n", bst_search(bst,memdata));
+//  if (bst_search(bst,memdata) == 1) {
+//    printf("%0d found\n", memdata.address);
+//  }
+//  else {
+//    printf("%0d not found\n", memdata.address);
+//  }
 }
 
-int main() {
+void testbst1() {
   node *bst;
   mem memdata;
+  printf("-------------------- start --------------------\n");
   memdata.address = 10;
   memdata.data = 0xAAA;
   bst = bst_new(memdata);
@@ -97,7 +128,43 @@ int main() {
   memdata.address = 12;
   memdata.data = 0xBBB;
   testsearch(bst,memdata);
+  bst_traverse(bst);
   bst_free(bst);
+  printf("-------------------- end --------------------\n");
+}
+
+void testbst2() {
+  node *bst = NULL;
+  mem memdata;
+  printf("-------------------- start --------------------\n");
+  memdata.address = 0;
+  memdata.data = 0xAAA;
+  bst = bst_insert(bst,memdata);
+  bst_traverse(bst);
+  memdata.address = 1;
+  memdata.data = 0xBBB;
+  bst = bst_insert(bst,memdata);
+  bst_traverse(bst);
+  //printf("bst->left->data.address: %0d\n", bst->left->data.address);
+  memdata.address = 2;
+  memdata.data = 0xCCC;
+  bst = bst_insert(bst,memdata);
+  bst_traverse(bst);
+  memdata.address = 0;
+  testsearch(bst,memdata);
+  memdata.address = 1;
+  testsearch(bst,memdata);
+  memdata.address = 2;
+  testsearch(bst,memdata);
+  
+  bst_traverse(bst);
+  bst_free(bst);
+  printf("-------------------- end --------------------\n");
+}
+
+int main() {
+  testbst1();
+  testbst2();
   return 0;  
 }
 #endif
